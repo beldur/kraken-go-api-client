2248fd451d4b8ec3a595d2ac90504676d862c9b4
Massimiliano Gerardi <massi.gerardi@gmail.com>
2020-01-25 21:27:45 +0100

    removed test for error response

--- krakenapi_test.go
+++ krakenapi_test.go
@@ -2,8 +2,6 @@ package krakenapi
 
 import (
 	"encoding/base64"
-	"github.com/jarcoal/httpmock"
-	"net/http"
 	"net/url"
 	"reflect"
 	"testing"
@@ -136,99 +134,5 @@ func TestQueryDepth(t *testing.T) {
 
 	if len(result.Bids) > count {
 		t.Errorf("Bids length must be less than count , got %d > %d", len(result.Bids), count)
-	}
-}
-
-func TestKrakenApi_TradeVolume(t *testing.T) {
-	type fields struct {
-		key    string
-		secret string
-		client *http.Client
-	}
-	type args map[string]string
-	tests := []struct {
-		name     string
-		fields   fields
-		args     args
-		response string
-		want     *TradeVolumeResponse
-		wantErr  bool
-	}{
-		{
-			name: "test_response",
-			fields: fields{
-				key:    "my_key",
-				secret: "my_secret",
-				client: http.DefaultClient,
-			},
-			args: args{
-				"pair":     XXBTZEUR,
-				"fee-info": "",
-			},
-			response: `{"error":[],"result":{"currency":"ZUSD","volume":"0.0000","fees":{"XXBTZEUR":{"fee":"0.2600","minfee":"0.1000","maxfee":"0.2600","nextfee":"0.2400","nextvolume":"50000.0000","tiervolume":"0.0000"}},"fees_maker":{"XXBTZEUR":{"fee":"0.1600","minfee":"0.0000","maxfee":"0.1600","nextfee":"0.1400","nextvolume":"50000.0000","tiervolume":"0.0000"}}}}`,
-			want: &TradeVolumeResponse{
-				Volume:   0.0000,
-				Currency: "ZUSD",
-				Fees: Fees{
-					XXBTZEUR: FeeInfo{
-						Fee:        0.2600,
-						MinFee:     0.1000,
-						MaxFee:     0.2600,
-						NextFee:    0.2400,
-						NextVolume: 50000.0000,
-						TierVolume: 0.0000,
-					},
-				},
-				FeesMaker: Fees{
-					XXBTZEUR: FeeInfo{
-						Fee:        0.1600,
-						MinFee:     0.0000,
-						MaxFee:     0.1600,
-						NextFee:    0.1400,
-						NextVolume: 50000.0000,
-						TierVolume: 0.0000,
-					},
-				},
-			},
-			wantErr: false,
-		},
-		{
-			name: "test_error",
-			fields: fields{
-				key:    "my_key",
-				secret: "my_secret",
-				client: http.DefaultClient,
-			},
-			args: args{
-				"pair":     XXBTZEUR,
-				"fee-info": "",
-			},
-			response: `{"error":["my_error"]}`,
-			want:     nil,
-			wantErr:  true,
-		},
-	}
-	for _, tt := range tests {
-		httpmock.Activate()
-		t.Run(tt.name, func(t *testing.T) {
-			api := &KrakenApi{
-				key:    tt.fields.key,
-				secret: tt.fields.secret,
-				client: tt.fields.client,
-			}
-			httpmock.RegisterResponder("POST", "https://api.kraken.com/0/private/TradeVolume", func(req *http.Request) (*http.Response, error) {
-				resp := httpmock.NewStringResponse(200, tt.response)
-				resp.Header.Add("Content-Type", "application/json")
-				return resp, nil
-			})
-			got, err := api.TradeVolume(tt.args)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("TradeVolume() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("TradeVolume() got = %v, want %v", got, tt.want)
-			}
-		})
 	}
 }
