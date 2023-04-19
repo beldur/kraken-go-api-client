9d20ca5131b102217d55366e5829fc508850b44f
Nikhil Saraf <1028334+nikhilsaraf@users.noreply.github.com>
2023-04-19 05:06:03 -0400

    feat(API): add USDCUSD market (#68)
    see https://api.kraken.com/0/public/Assets
    and https://api.kraken.com/0/public/AssetPairs?pair=USDCUSD
    
    Are there tests that will let me confirm this works for all endpoints?

--- types.go
+++ types.go
@@ -38,6 +38,7 @@ const (
 	QTUMEUR  = "QTUMEUR"
 	QTUMUSD  = "QTUMUSD"
 	QTUMXBT  = "QTUMXBT"
+	USDCUSD  = "USDCUSD"
 	USDTZUSD = "USDTZUSD"
 	XBTUSDT  = "XBTUSDT"
 	XETCXETH = "XETCXETH"
@@ -139,6 +140,7 @@ type AssetPairsResponse struct {
 	QTUMEUR  AssetPairInfo
 	QTUMUSD  AssetPairInfo
 	QTUMXBT  AssetPairInfo
+	USDCUSD  AssetPairInfo
 	USDTZUSD AssetPairInfo
 	XETCXETH AssetPairInfo
 	XETCXXBT AssetPairInfo
@@ -237,6 +239,7 @@ type AssetsResponse struct {
 	KFEE AssetInfo
 	LINK AssetInfo
 	QTUM AssetInfo
+	USDC AssetInfo
 	USDT AssetInfo
 	XDAO AssetInfo
 	XETC AssetInfo
@@ -285,6 +288,7 @@ type BalanceResponse struct {
 	QTUM float64 `json:"QTUM,string"`
 	KFEE float64 `json:"KFEE,string"`
 	LINK float64 `json:"LINK,string"`
+	USDC float64 `json:"USDC,string"`
 	USDT float64 `json:"USDT,string"`
 	XDAO float64 `json:"XDAO,string"`
 	XETC float64 `json:"XETC,string"`
@@ -353,6 +357,7 @@ type Fees struct {
 	QTUMEUR  FeeInfo
 	QTUMUSD  FeeInfo
 	QTUMXBT  FeeInfo
+	USDCUSD  FeeInfo
 	USDTZUSD FeeInfo
 	XETCXETH FeeInfo
 	XETCXXBT FeeInfo
@@ -449,6 +454,7 @@ type TickerResponse struct {
 	QTUMEUR  PairTickerInfo
 	QTUMUSD  PairTickerInfo
 	QTUMXBT  PairTickerInfo
+	USDCUSD  PairTickerInfo
 	USDTZUSD PairTickerInfo
 	XBTUSDT  PairTickerInfo
 	XETCXETH PairTickerInfo
